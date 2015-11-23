5373a69913410071648a24cb43f4a0130e51d22f
mdpollo2 <mdpollo2@ncsu.edu>
2015-11-23 11:02:39 -0500

    Added tests for doctor creating surgeries

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/OphthalmologySurgeryTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/OphthalmologySurgeryTest.java
@@ -46,8 +46,6 @@ public class OphthalmologySurgeryTest extends iTrustSeleniumTest{
 		
 		//Add surgery data
 		new Select(driver.findElement(By.name("surgeryID"))).selectByValue("1");
-
-		//Add Ophthalmology Data
 		driver.findElement(By.id("surgeryNotes")).clear();
 		driver.findElement(By.id("surgeryNotes")).sendKeys("This is a hold up");
 		
@@ -57,6 +55,47 @@ public class OphthalmologySurgeryTest extends iTrustSeleniumTest{
 		//driver.findElement(By.xpath(".//*[@value='Add Surgery Data']")).submit();
 				
 		assertTrue(driver.getPageSource().contains("Cataract Surgery"));
+	}
+	
+	//test no surgery type
+	public void testNoSurgeryType() throws Exception{
+		// login HCP Dr Laser
+		HtmlUnitDriver driver = (HtmlUnitDriver)this.login("9900000023", "pw");
+		assertLogged(TransactionType.HOME_VIEW, 9900000023L, 0L, "");
+		
+		assertTrue(driver.getPageSource().contains("iTrust - HCP Home"));
+		//find patient
+		driver.findElement(By.linkText("Document Office Visit")).click();
+		
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("15");
+		driver.findElement(By.xpath("//input[@value='15']")).submit();
+		//Verify Document Office Visit page
+		assertEquals(ADDRESS + "auth/hcp-uap/documentOfficeVisit.jsp", driver.getCurrentUrl());
+		
+		//On the correct page 
+		assertEquals("iTrust - Document Office Visit" , driver.getTitle());
+		
+		driver.findElement(By.xpath(".//*[@value='Yes, Document Office Visit']")).click();
+		//Add date
+		driver.findElement(By.name("visitDate")).clear();
+		driver.findElement(By.name("visitDate")).sendKeys("10/14/2015");
+		
+		new Select(driver.findElement(By.name("apptType"))).selectByValue("Ophthalmology Surgery");
+		
+		driver.findElement(By.id("update")).click();
+		
+		assertTrue(driver.getPageSource().contains("Surgery"));
+		
+		//Add Surgery Data
+		driver.findElement(By.id("surgeryNotes")).clear();
+		driver.findElement(By.id("surgeryNotes")).sendKeys("This is a hold up");
+		
+		
+		//Submit form
+		driver.findElement(By.xpath(".//*[@value='Add Surgery Data']")).click();
+		//driver.findElement(By.xpath(".//*[@value='Add Surgery Data']")).submit();
+				
+		assertTrue(driver.getPageSource().contains("Must select a Surgery Type"));
 	}
 	
 }
